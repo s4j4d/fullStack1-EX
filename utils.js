3291a98b22080aa9c69bc0a094da5417497220cd
Ahooraam <ahoora.amini8@gmail.com>
2023-02-26 09:24:19 +0330

    utils module added to project

---
+++ utils.js
@@ -0,0 +1,48 @@
+const fs = require("fs"),
+{ StatusCodes}= require("http-status-codes");
+const {resolve}= require("path")
+
+exports.getFile = (path, res) =>{
+    fs.readFile(path, (err, data) => {
+        if(err){
+            res.writeHead(StatusCodes.INTERNAL_SERVER_ERROR);
+            res.end(err);
+        }else{
+            res.end(data)
+        }
+    });
+};
+
+exports.errResponse = (res, errMessage) => {
+    const result = {
+        error: true,
+        message : errMessage
+    };
+    return res.json(result);
+};
+
+exports.parseJsonBody = (req) => {
+    return new Promise((resolve)=>{
+        let body = [];
+        req
+            .on('data', (chunk)=>{
+                body.push(chunk);
+            })
+            .on('end', ()=>{
+                body = Buffer.concat(body).toString();
+                resolve(JSON.parse(body))
+            });
+    });
+};
+
+exports.jsonSerialize = (data, res) => {
+    res.end(JSON.stringify(data));
+}
+
+exports.parseQueryFromUrl = (url) => {
+    if(!url) return {};
+    return url.split("&").reduce((prevVal, currVal)=>{
+        const [key,val]= currVal.split("=");
+        return {...prevVal, [key]: val}
+    }, {})
+}
