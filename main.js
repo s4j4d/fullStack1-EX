305718a12c2faa7aaa0ecf289111632771517adb
Ahooraam <ahoora.amini8@gmail.com>
2023-02-26 16:47:48 +0330

    minor fix to main , utils , routes and adding prettier

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const http = require("http"),
-route = require("./routes"),
-utils = require("./utils"),
-route = require('./routes')
+  utils = require("./utils"),
+  { contentTypes } = require("./content-types"),
+  route = require("./routes"),
+  userController = require("./controllers/user.controller"),
+  bookController = require('./controllers/book.controller')
 
 const PORT = process.env.PORT || 3000;
 
 // user routes : register , user-detail - user-list -user books
 
+route.post("/user/register", userController.register);
+route.get("/user/user-detail", {});
+route.get("/user/user-list", {});
+route.get("/user/user-books", {});
 
 // book routes : add book , lend book , book list, book detail
 
-
-const server = http.createServer(route.handler)
-server.listen(PORT, ()=>{
-    console.log(`run server on port ${PORT}`);
-})
+const server = http.createServer(route.handler);
+server.listen(PORT, () => {
+  console.log(`run server on port ${PORT}`);
+});
--- routes.js
+++ routes.js
@@ -1,51 +1,47 @@
-const {StatusCodes} =require('http-status-codes'),
-url = require('url-parse'),
-utils = require('./utils');
+const { StatusCodes } = require("http-status-codes");
+const url = require("url");
+const utils = require("./utils");
 
-const routing ={
-    "GET": {},
-    "POST": {},
-    "PUT": {},
-    "PATCH": {},
-    "DELETE": {}
+const routing = {
+  GET: {},
+  POST: {},
+  PUT: {},
+  PATCH: {},
+  DELETE: {},
 };
 
 exports.handler = async (req, res) => {
   try {
     req.path = url.parse(req.url).pathname;
-    req.query = utils.parseQueryFromUrl(url.parse(req.url). query);
-    res.json=(data) => utils.jsonSerialize(data, res);
-    req.body = null
+    req.query = utils.parseQueryFromUrl(url.parse(req.url).query);
+    res.json = (data) => utils.jsonSerialize(data, res);
+    req.body = null;
 
-    if(req.header["content-type"] === "application/js")
-      req.body = await utils.parseJsonBody(req);  
-    
+    if (req.headers["content-type"] === "application/json")
+      req.body = await utils.parseJsonBody(req);
     routing[req.method][req.path](req, res);
-    
   } catch (error) {
     res.writeHead(StatusCodes.NOT_FOUND);
     return utils.errResponse(res, error.message);
   }
-}
-
+};
 
 exports.get = (url, action) => {
-    routing.GET[url] = action;
-  };
-  
-  exports.post = (url, action) => {
-    routing.POST[url] = action;
-  };
-  
-  exports.put = (url, action) => {
-    routing.PUT[url] = action;
-  };
-  
-  exports.patch = (url, action) => {
-    routing.PATCH[url] = action;
-  };
-  
-  exports.delete = (url, action) => {
-    routing.DELETE[url] = action;
-  };
-  
+  routing.GET[url] = action;
+};
+
+exports.post = (url, action) => {
+  routing.POST[url] = action;
+};
+
+exports.put = (url, action) => {
+  routing.PUT[url] = action;
+};
+
+exports.patch = (url, action) => {
+  routing.PATCH[url] = action;
+};
+
+exports.delete = (url, action) => {
+  routing.DELETE[url] = action;
+};
