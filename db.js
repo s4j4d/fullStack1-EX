e6dd570ac4351ff49cab848ae46234aec3be57fc
Ahooraam <ahoora.amini8@gmail.com>
2023-02-26 16:49:43 +0330

    changing the structure of book and user  tables

--- db.js
+++ db.js
@@ -1,27 +1,33 @@
 const sqlite3 = require("sqlite3").verbose();
 const DBFILENAME = "libraryDB.sqlite3";
 
-const db = new sqlite3.Database(DBFILENAME,(err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("connected to db...");
+const db = new sqlite3.Database(DBFILENAME, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected to db...");
 
-        db.serialize(()=>{
-            db.run(`CREATE TABLE IF NOT EXISTS Book(
-                        bookID INTEGER PRIMARY KEY,
-                        number INTEGER NOT NULL,
+    db.serialize(() => {
+      db.run(`CREATE TABLE IF NOT EXISTS User(
+                          userID INTEGER PRIMARY KEY,
+                          username TEXT NOT NULL UNIQUE ,
+                          password TEXT 
+              )`);
+      db.run(`CREATE TABLE IF NOT EXISTS Book(
+                        specialID INTEGER PRIMARY KEY UNIQUE,
+                        bookID INTEGER ,
+                        entity INTEGER NOT NULL,
                         title TEXT NOT NULL,
-                        description TEXT 
+                        author TEXT NOT NULL,
+                        description TEXT ,
+                        isBorrowed BOOLEAN NOT NULL ,
+                        borrowerID INTEGER
+
+                        
                         
             )`);
-            db.run(`CREATE TABLE IF NOT EXISTS User(
-                        userID INTEGER NOT NULL UNIQUE,
-                        username TEXT NOT NULL ,
-                        password TEXT 
-            )`)
-        })
-    }
-})
+    });
+  }
+});
 
 module.exports = db;
